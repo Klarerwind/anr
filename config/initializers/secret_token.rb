930decb71315de665cd183d9b5f7891db418eb08
Stephen Lam <szl2f@virginia.edu>
2013-07-26 00:57:16 -0400

    Starting over with new project.
    Going to use Mongoid with MongoDB so starting over with --skip-active-record.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anr::Application.config.secret_key_base = '201fe357c0eb3059317e177ea6d0f6e7ba81d0a5810b05757f79224b89ab4d0ba4784a2264516738dc5f196c3dc21119abd5b92280c2e54b56487a82d512b459'
